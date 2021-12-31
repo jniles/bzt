bd21aee01e4693e61a753269e3f7ad8d024d247e
Jonathan Niles <jonathanwniles@gmail.com>
2021-12-31 08:19:28 +0100

    chore: ensure .env readable

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import _ from 'dotenv/config';
-import readFile from 'fs';
-import enquire from 'enquirer';
-import debug from 'debug';
+#!/usr/bin/env node
 
+import dotenv from 'dotenv';
+import enquire from 'enquirer';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+// alias __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path : path.join(__dirname, '.env') });
+
+import debug from 'debug';
 import { getDefaultNetwork, getOnlineClients } from './lib/zt.js';
 import { tunnelToServer } from './lib/tunnel.js';
 
-const { Select } = enquire;
 const { ZT_TOKEN, ZT_NETWORK_ID } = process.env;
 
-const log = debug('bzt');
+const { Select } = enquire;
+
 
 async function main() {
+  const log = debug('bzt');
   try {
     log(`Using network: ${ZT_NETWORK_ID}.`);
 
